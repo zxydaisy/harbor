f2578813f0f14f85cd8b75cd1201b66ca538c2a7
Xiaoyu <zxydaisy@caicloud.io>
2016-07-15 22:34:30 +0800

    add label

--- Deploy/rebuild.sh
+++ Deploy/rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose stop
+docker-compose -f docker-compose-zxy.yml down
 docker ps -a|grep ago | awk '{print $1}' |xargs docker rm
 docker rmi deploy_ui
-docker-compose up -d
+docker-compose -f docker-compose-zxy.yml up -d
