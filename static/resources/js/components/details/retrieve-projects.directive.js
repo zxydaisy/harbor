333ebd2c2161eca55c258d71fa6e6a8e8638bcbf
Xiaoyu <zxydaisy@caicloud.io>
2016-07-26 15:47:04 +0800

    add search

--- static/resources/js/components/details/retrieve-projects.directive.js
+++ static/resources/js/components/details/retrieve-projects.directive.js
@@ -3,9 +3,9 @@
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-        
+
         http://www.apache.org/licenses/LICENSE-2.0
-        
+
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,21 +13,22 @@
     limitations under the License.
 */
 (function() {
-  
+
   'use strict';
-  
+
   angular
     .module('harbor.details')
     .directive('retrieveProjects', retrieveProjects);
-  
-  RetrieveProjectsController.$inject = ['$scope', 'nameFilter', '$filter', 'trFilter', 'ListProjectService', '$location', 'getParameterByName', 'CurrentProjectMemberService', '$window'];
-   
-  function RetrieveProjectsController($scope, nameFilter, $filter, trFilter, ListProjectService, $location, getParameterByName, CurrentProjectMemberService, $window) {
+
+  RetrieveProjectsController.$inject = ['$scope', 'nameFilter', '$filter', 'trFilter', 'ListProjectService', 'ListCustomService', '$location', 'getParameterByName', 'CurrentProjectMemberService', '$window'];
+
+  function RetrieveProjectsController($scope, nameFilter, $filter, trFilter, ListProjectService, ListCustomService, $location, getParameterByName, CurrentProjectMemberService, $window) {
     var vm = this;
-    
+
     vm.projectName = '';
     vm.isOpen = false;
-    
+    vm.customMap = {};
+
     if(getParameterByName('is_public', $location.absUrl())) {
       vm.isPublic = getParameterByName('is_public', $location.absUrl()) === 'true' ? 1 : 0;
       vm.publicity = (vm.isPublic === 1) ? true : false;
@@ -35,28 +36,42 @@
 
     vm.retrieve = retrieve;
     vm.filterInput = '';
-    vm.selectItem = selectItem;  
-    vm.checkProjectMember = checkProjectMember;  
-       
+    vm.selectItem = selectItem;
+    vm.checkProjectMember = checkProjectMember;
+
     $scope.$watch('vm.selectedProject', function(current, origin) {
-      if(current) {        
+      if(current) {
         vm.selectedId = current.project_id;
+        ListCustomService(vm.selectedId);
       }
     });
-       
-    $scope.$watch('vm.publicity', function(current, origin) { 
+
+    $scope.$watch('vm.publicity', function(current, origin) {
       vm.publicity = current ? true : false;
       vm.isPublic =  vm.publicity ? 1 : 0;
       vm.projectType = (vm.isPublic === 1) ? 'public_projects' : 'my_projects';
-      vm.retrieve();      
+      vm.retrieve();
     });
-       
+
+    function getCustoms(projectId){
+      //根据项目id获取对应的客户
+      ListCustomService(projectId)
+        .success(function(data, status) {
+          vm.customMap[projectId] = data || [];
+        })
+        .error(getCustomFailed);
+    }
+
+    function getCustomFailed(response) {
+      console.log('Failed to list repositories:' + response);
+    }
+
     function retrieve() {
       ListProjectService(vm.projectName, vm.isPublic)
         .success(getProjectSuccess)
         .error(getProjectFailed);
     }
-    
+
     function getProjectSuccess(data, status) {
       vm.projects = data;
 
@@ -67,66 +82,66 @@
         console.log('vm.projects is null, load public projects.');
         return;
       }
-      
+
       if(angular.isArray(vm.projects) && vm.projects.length > 0) {
-        vm.selectedProject = vm.projects[0];    
+        vm.selectedProject = vm.projects[0];
       }else{
-        $window.location.href = '/project';        
+        $window.location.href = '/project';
       }
-      
+
       if(getParameterByName('project_id', $location.absUrl())){
         angular.forEach(vm.projects, function(value, index) {
           if(value['project_id'] === Number(getParameterByName('project_id', $location.absUrl()))) {
             vm.selectedProject = value;
           }
-        }); 
+        });
       }
-     
+
       $location.search('project_id', vm.selectedProject.project_id);
-      
-      vm.checkProjectMember(vm.selectedProject.project_id);      
+
+      vm.checkProjectMember(vm.selectedProject.project_id);
       vm.resultCount = vm.projects.length;
-    
-      $scope.$watch('vm.filterInput', function(current, origin) {  
+
+      $scope.$watch('vm.filterInput', function(current, origin) {
         vm.resultCount = $filter('name')(vm.projects, vm.filterInput, 'name').length;
       });
     }
-    
+
     function getProjectFailed(data) {
       $scope.$emit('modalTitle', $filter('tr')('error'));
       $scope.$emit('modalMessage', $filter('tr')('failed_to_get_project'));
       $scope.$emit('raiseError', true);
       console.log('Failed to list projects.');
     }
-      
+
     function selectItem(item) {
       vm.selectedProject = item;
       $location.search('project_id', vm.selectedProject.project_id);
-    }       
-  
+    }
+
     $scope.$on('$locationChangeSuccess', function(e) {
       var projectId = getParameterByName('project_id', $location.absUrl());
-      vm.isOpen = false;   
+      vm.isOpen = false;
     });
-    
+
     function checkProjectMember(projectId) {
       CurrentProjectMemberService(projectId)
         .success(getCurrentProjectMemberSuccess)
         .error(getCurrentProjectMemberFailed);
     }
-    
+
     function getCurrentProjectMemberSuccess(data, status) {
       console.log('Successful get current project member:' + status);
       vm.isProjectMember = true;
     }
-    
+
     function getCurrentProjectMemberFailed(data, status) {
-      vm.isProjectMember = false;           
+      vm.isProjectMember = false;
       console.log('Current user has no member for the project:' + status +  ', location.url:' + $location.url());
     }
-    
+
   }
-  
+
   function retrieveProjects() {
     var directive = {
       restrict: 'E',
@@ -142,25 +157,25 @@
       bindToController: true,
       controllerAs: 'vm'
     };
-    
+
     return directive;
-    
+
     function link(scope, element, attrs, ctrl) {
       $(document).on('click', clickHandler);
-    
+
       function clickHandler(e) {
-        $('[data-toggle="popover"]').each(function () {          
-          if (!$(this).is(e.target) && 
+        $('[data-toggle="popover"]').each(function () {
+          if (!$(this).is(e.target) &&
                $(this).has(e.target).length === 0 &&
                $('.popover').has(e.target).length === 0) {
              $(this).parent().popover('hide');
           }
         });
         var targetId = $(e.target).attr('id');
-        if(targetId === 'switchPane' || 
+        if(targetId === 'switchPane' ||
            targetId === 'retrievePane' ||
            targetId === 'retrieveFilter') {
-          return;            
+          return;
         }else{
           ctrl.isOpen = false;
           scope.$apply();
@@ -169,5 +184,5 @@
     }
 
   }
-  
+
 })();
--- static/resources/js/components/repository/list-repository.directive.js
+++ static/resources/js/components/repository/list-repository.directive.js
@@ -60,10 +60,12 @@
    };
 
     vm.projectId = getParameterByName('project_id', $location.absUrl());
+    vm.customId = getParameterByName('custom_Id', $location.absUrl());
     vm.retrieve();
 
     $scope.$on('$locationChangeSuccess', function() {
       vm.projectId = getParameterByName('project_id', $location.absUrl());
+      vm.customId = getParameterByName('custom_Id', $location.absUrl());
       vm.filterInput = '';
       vm.retrieve();
     });
